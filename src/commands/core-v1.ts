2d06951491c7f0b205eac0b24a74d25abd349023
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-21 13:24:28 -0600

    feat: add flags and args

--- src/commands/core-v1.ts
+++ src/commands/core-v1.ts
@@ -1,7 +1,40 @@
-import {Command} from '@oclif/core'
+import {Command, Flags, Interfaces} from '@oclif/core'
+
+type Result = {
+  args: { [name: string]: any }
+  flags: Interfaces.InferredFlags<typeof CoreV1.flags>
+}
 
 export default class CoreV1 extends Command {
-  public async run(): Promise<void> {
+  static flags = {
+    optionalString: Flags.string(),
+    defaultString: Flags.string({
+      default: 'simple string default',
+    }),
+    defaultFnString: Flags.string({
+      default: async () => Promise.resolve('async fn default'),
+    }),
+  }
+
+  static args = [
+    {
+      name: 'optionalArg',
+    },
+    {
+      name: 'defaultArg',
+      default: 'simple string default',
+    },
+    {
+      name: 'defaultFnArg',
+      default: async (): Promise<string> => Promise.resolve('async fn default'),
+    },
+  ]
+
+  static enableJsonFlag = true
+
+  async run(): Promise<Result> {
+    const {args, flags} = await this.parse(CoreV1)
     this.log(`hello I am an @oclif/core@v1 plugin from ${this.config.root}!`)
+    return {args, flags}
   }
 }
